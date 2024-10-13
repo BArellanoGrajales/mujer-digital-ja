07106780ee091341aab1122f83974b3d71b0feb7
BeatrizA <barellano424@estudiantes.ciad.mx>
2024-10-13 16:38:36 -0700

    avance del modulo 2 / Intro a JS y la consola

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,4 +1,17 @@
 
+/* alert('Hola Mundo'); */
 
+/* console.log( miNombre ); */
 
-console.log( miNombre );
+/* prompt( '¿Cual es tu nombre?', 'sin nombreb' ); */
+
+/* let nombre = prompt('¿Cual es tu nombre?', 'Sin nombre'); */
+
+/* let nombre = prompt('¿Cual es tu nombre?');
+console.log( nombre ); // ESto hace que se agrege un Null cuando 
+console.log( '****' + nombre + '****' );  // esto es un sting vasio, no es un undefine o un valor nulo
+ */
+
+/* const seleccion = confirm ('¿Esta seguro de borrar esto?'); //  esto no corre en la terminal local de node unicamente en el navegador web
+console.log( seleccion ); // esto no corre en la terminal local de node unicamente en el navegador web */
+
