3baf045d2c5e29ca302301c93a831811549558a6
BeatrizA <barellano424@estudiantes.ciad.mx>
2024-10-21 12:47:50 -0700

    creación de carpeta de arreglos

--- 01-fundamentos/assets/js/palabras-reservadas.js
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -1,1 +1,26 @@
 
+// el estandar de javascript
+
+let obbjeto$ = 123;
+let objeto123.123= 123; // el punto significa que  quiero acceder a alguna propiedad o metodo propio de este objeto
+// es imporante ponerle nombre de las variables algo significativo
+
+
+
+//Practica de Camel Case
+
+/* ## Ejemplos prácticos:
+```javascript
+// Lower Camel Case (usado para variables y funciones)
+let totalPrice = 0;
+function calculateTotalPrice() {
+    return totalPrice;
+}
+
+// Upper Camel Case (usado para clases)
+class ShoppingCart {
+    constructor() {
+        this.items = [];
+    }
+}
+ */
