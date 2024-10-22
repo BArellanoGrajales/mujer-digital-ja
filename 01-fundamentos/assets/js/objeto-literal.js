d6dbf38476a70a7708148f53eb3021e97b304b50
BeatrizA <barellano424@estudiantes.ciad.mx>
2024-10-21 17:34:41 -0700

    propiedades y metodos -  Repaso javaScript

--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -15,7 +15,7 @@ let personaje = {
         zip: '10880, 90265',
         ubicacion: 'Malibu, California'
     },
-    'ultima-pelicula': 'Infinity War'
+    'ultima-pelicula': 'Infinity War' // el ponerlo entre comillas es una estartegia para decirle  a la mauqina que eso es un String
 };
 
 console.log(personaje);
@@ -28,6 +28,19 @@ console.log('Lat', personaje.coords.lat);
 //ejercicio, dime le numero de trajes que tiene Airoman
 // yo se que la repsuesta es 3, pero imagenemos que no lo sabemos
 console.log('No. Trajes', personaje.trajes.length );
+
+//Acceder al último elemento del array: Para obtener el último elemento de un array, puedes restar 1 al valor de .length. 
+console.log('ultimo traje', personaje.trajes[personaje.trajes.length - 1]);  //  el índice del último elemento siempre será igual a longitud del array - 1 (ya que los índices comienzan desde 0).
+/* Uso de console.log:
+La función console.log() se usa para imprimir el resultado en la consola. Aquí imprime dos cosas:
+
+'ultimo traje': Un mensaje en texto que dice "último traje".
+personaje.trajes[personaje.trajes.length - 1]: El valor del último traje, que en este caso es 'Hulkbuster'. */
+
+const x= 'vivo';
+console.log('Vivo:', personaje[x]); // aqui se imprime el valor de la propiedad vivo que es false
+
+console.log('Ultima pelicula:', personaje['ultima-pelicula']);
 
 
 
